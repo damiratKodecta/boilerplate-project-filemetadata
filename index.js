fe6fab33a06eb6bc935099c325b93defcdf5e25f
Damir Omerasevic <damir.omerasevic@kodecta.com>
2024-09-23 16:39:12 +0000

    using multer

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
